f63ec31ce7f3572c393fb892ada2518ec9f15a1a
bouzuya <m@bouzuya.net>
2016-05-17 23:38:56 +0900

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to my-cljs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/my_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [my-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
